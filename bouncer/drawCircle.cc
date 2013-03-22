742b6b63bf55e8e52899c9bbf00e5c7ef25635d5
Josh <joshadambell@me.com>
2013-03-22 11:34:17 -0600

    formatting changes

--- bouncer/drawCircle.cc
+++ bouncer/drawCircle.cc
@@ -10,27 +10,34 @@
 // so I think if we just move the y value
 
 // function that takes the frame we are going to draw the circle on.
-AVFrame drawCircle(AVFrame *p)
+AVFrame drawCircle(AVFrame *pict)
 {
+
+
+  uint8_t *buf;
+  int height;
+  int width;
+  height = pict->height; // height of frame
+  width = pict->width; // width of frame
+
+  // we could start in the center of the picture
+  int center = width/2;
+
+  //we could make a complete center coordinates. maybe a p
+  int centerx = width/2;
+  int centery = height/2;
+
+  // lets start by adding an entire width of pixels
+  for(int row = 0; row < width; row++)
+    {
+
+      uint8_t * pixel = pict->data[0]
+  // do what we did in our encoding. create a pixel and add it to our bytestream
+  bytestream_put_byte(&buf, pixel[0]);
+      // this seems like it will just go at the end of our picture. How do we get it
+      // to go in to the position that we want it to
+
+  //how do we change the set the color of a pixel??????
+    }
+ 
 }
-
-uint8_t *buf;
-int height;
-int width;
-height = p->height; // height of frame
-width = p->width; // width of frame
-
-// we could start in the center of the picture
-int center = width/2;
-
-//we could make a complete center coordinates. maybe a p
-int centerx = width/2;
-int centery = height/2;
-
-// lets start by adding say width/3 number of pixels to a line
-for(int row = 0; row < 
-
-// do what we did in our encoding. create a pixel and add it to our bytestream
-bytestream_put_byte(&buf, pixel[0]);
-
-//how do we change the set the color of a pixel??????
