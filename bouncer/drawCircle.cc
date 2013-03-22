c6789b1f6acc5861044830166f339c79d1cd165e
Josh <joshadambell@me.com>
2013-03-22 12:10:36 -0600

    I can't get it to compile.
    g++ -I ../ffmpeg/include/ -L ../ffmpeg/lib bouncer.cc `pkg-config
    --cflags --libs libavutil libavformat libavcodec`
    
    bouncer.cc:5:21: error: avcodec.h: No such file or directory
    bouncer.cc:6:22: error: avformat.h: No such file or directory

--- bouncer/drawCircle.cc
+++ bouncer/drawCircle.cc
@@ -31,8 +31,8 @@ AVFrame drawCircle(AVFrame *pict)
   for(int col = 0; col < width; col++)
     {
 
-      uint8_t * pixel = pict->data[0] + (height/2 * p->linesize[0] + col;
-  // do what we did in our encoding. create a pixel and add it to our bytestream
+      uint8_t * pixel = pict->data[0] + (height/2 * p->linesize[0]) + col;
+      // do what we did in our encoding. create a pixel and add it to our bytestream
   bytestream_put_byte(&buf, pixel[0]);
       // this seems like it will just go at the end of our picture. How do we get it
       // to go in to the position that we want it to
